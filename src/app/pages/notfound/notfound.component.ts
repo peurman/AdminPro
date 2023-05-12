5fc41fff84f743f68299767d9494dbc2726de0ed
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-05-12 18:45:53 -0300

    add code and styles in login, register and notfound pages

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./notfound.component.css'],
 })
 export class NotfoundComponent {
-
+  year = new Date().getFullYear();
 }
