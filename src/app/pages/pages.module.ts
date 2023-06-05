d7674783798d7e7b2f6b49377da1fb30edac1ff9
Esteban Manrupe <esteban_manrupe@hotmail.com>
2023-06-05 10:57:49 -0300

    update progress page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { PagesComponent } from './pages.component';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, FormsModule, SharedModule],
   exports: [
     DashboardComponent,
     ProgressComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
+  progress: number = 50;
 
+  get getPercentage() {
+    return `${this.progress}%`;
+  }
+
+  changeValue(value: number) {
+    if (this.progress >= 100 && value > 0) return (this.progress = 100);
+    if (this.progress <= 0 && value < 0) return (this.progress = 0);
+    return (this.progress = this.progress + value);
+  }
 }
